79efdad3d0e6327140eaa3720198bde438d9e7fe
BT <tekwanib7@gmail.com>
2026-06-09 20:17:41 +0100

    Enable TypeScript ESLint parser

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import js from "@eslint/js";
 import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default [
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ["**/*.{js,ts}"],
     languageOptions: {
@@ -15,7 +17,8 @@ export default [
       }
     },
     rules: {
-      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
       "no-undef": "off"
     }
   }
